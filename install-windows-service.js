d337198174ce13c2a29c472c03ebe1435fd8c687
Hung_Nguyen <hung@agencyrevolution.com>
2014-03-25 18:36:29 +0700

    Write log and update module versions

--- install-windows-service.js
+++ install-windows-service.js
@@ -1,29 +1,37 @@
 var Service = require('node-windows').Service;
 var Path = require("path");
 
+var nodeAppPath = Path.resolve('./powertemplate.js');
+console.log(nodeAppPath);
+
 // Create a new service object
 var svc = new Service({
-  name: 'Agency Rev Power Template Engine',
-  description: 'Using Handlebars to convert template and JSON to Html content.',
-  script: Path.resolve('./powertemplate.js'),
-  wait: 2,
-  grow: 0.5,
-  maxRetries: 3
+	name: 'Agency Rev Power Template Engine',
+	description: 'Using Handlebars to convert template and JSON to Html content.',
+	script: nodeAppPath,
+	wait: 2,
+	grow: 0.5,
+	maxRetries: 3
 });
 
 // Listen for the "install" event, which indicates the process is available as a service.
 svc.on('install', function() {
-  svc.start();
+	console.log('Starting service...');
+	svc.start();
+	console.log('Service started');
 });
 
 // Listen for the "uninstall" event so we know when it's done.
 svc.on('uninstall',function() {
-  console.log('Uninstall complete.');
-  console.log('The service exists: ', svc.exists);
+	console.log('Uninstall complete.');
+	console.log('The service exists: ', svc.exists);
 });
 
 // install the service
-svc.install();
+setTimeout(function() {
+	console.log('Installing service...');
+	svc.install();
+}, 3000);
 
 // Uninstall the service.
 //svc.uninstall();
